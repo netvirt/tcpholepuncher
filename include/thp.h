ab35ca946c509f780be5f5fb00d54beeaf9f8182
Nicolas J. Bouliane <nicboul@gmail.com>
2018-10-07 00:06:54 +0000

    [THP-3] rewrite log subsystem and add first test

--- include/thp.h
+++ include/thp.h
@@ -3,27 +3,8 @@
 
 #include <stdarg.h>
 
-void    thp_list_test();
+typedef void (*cb_log)(const char *);
 
-/*
- * Call_backs for logging
- */
-typedef void(* info_logger_cb)(const char *, ...);
-typedef void(* warn_logger_cb)(const char *, ...);
-typedef void(* debug_logger_cb)(const char *, ...);
-typedef void(* fatal_logger_cb)(const char *);
+void	 log_set_cb(cb_log);
 
-extern const char	*log_procname;
-
-void	log_init(int);
-void	log_verbose(int);
-void    vlog(int pri, const char *, va_list);
-void    logit(int pri, const char *, ...);
-void    log_warn(const char *, ...);
-void    log_warnx(const char *, ...);
-void    log_info(const char *, ...);
-void    log_debug(const char *, ...);
-void    fatal(const char *);
-void    fatalx(const char *);
-
-#endif /* THP_H */
+#endif
